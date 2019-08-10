fb1d30f5d047e40b0a0ccd0010ff332bcc32f86a
33marvil <martinramirezcruz71@gmail.com>
2019-08-10 11:53:51 -0500

    Schema-Model-User

--- src/models/User.js
+++ src/models/User.js
@@ -1,2 +1,9 @@
+const mongoose = require('mongoose');
 
+const Schema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String},
+    email: {type: String, validate: [validatePresenceOf, "email require)"], index: {unique: true} }
+});
 
+module.exports = mongoose.model('User', Schema);
