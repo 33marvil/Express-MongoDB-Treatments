707d5e1223e2fd261b4a4d286d0177e72ad09d85
33marvil <martinramirezcruz71@gmail.com>
2019-08-12 16:32:57 -0500

    Endpoint-Post-endpoint-treatments-v2

--- src/routes/api.js
+++ src/routes/api.js
@@ -23,9 +23,9 @@ app.put('/users/:id', container.get('updateDataController', User));
 
 
 // Testing Endpoint Treatments
-app.post('/treatments', container.get('createDataController', Treatment , Appointment));
-app.get('/treatments', container.get('listDataController', Treatment));
-app.get('/treatments/:id', container.get('listDataByIdController', Treatment));
+app.post('/treatments', container.get('Treatments/treatments', Treatment , Appointment));
+// app.get('/treatments', container.get('listDataController', Treatment));
+// app.get('/treatments/:id', container.get('listDataByIdController', Treatment));
 
 
 module.exports = app;
