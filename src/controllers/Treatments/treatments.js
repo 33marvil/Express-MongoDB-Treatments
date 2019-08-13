01f7f4e2c9d6284efb61c5c89a03186f89fa2461
33marvil <martinramirezcruz71@gmail.com>
2019-08-13 15:06:33 -0500

    Endpoint-Treatment-Validate-User-v1

--- src/controllers/Treatments/treatments.js
+++ src/controllers/Treatments/treatments.js
@@ -1,47 +1,42 @@
 const mongoose = require('mongoose');
 const Treatment = require('../../models/Treatment');
 
+const User = require('../../models/User');
+console.log(User)
+
 const controller = {
         create: (req, res) => {
-        // Recibir del body 1a parte
-            
+        // Receive body data 1a parte
         const description = req.body.description;
-            console.log(description);
+            // console.log(description);
         const listOfTreatments = req.body.listOfTreatments.split(",");
-            console.log(listOfTreatments);
-        const user = req.body.user;
-            console.log(user);
+            // console.log(listOfTreatments);
+        const userId = req.body.user;
+            // console.log(user);
+        // existe el usuario que recivo del body?
+        // si existe el usuario
+        User
+            .findById(userId)
+            .exec()
+            .then(user => {
+                console.log(user)
+            })
+            .catch(err => {
+                console.log(`caugth error: ${err}`);
+                return res.status(500).json(err);
+            })
+            
+        // crear  treatment y lo grabo con los datos que tengo
+        // utilizo propiedad de Treatment -- appointments:  push[id appointment]
+
+
+        // Formar el Objeto Treatment
         
-    
-        // newTreatment = {
-        //     _id: mongoose.Schema.Types.ObjectId,
-        //     description: { type: String },
-            // listOfTreatmen
-        //     appointments: {
-        //         name: { type: String },
-        //         phoneNumber: { type: Number },
-        //         day: { type: String }
-        //     }
-        // };
-        // list.pop(list);
-        // console.log(newTreatment);
-        // list.push(newTreatment);
-        
-        // newTreatment
-        //     .save()
-        //     .then(data => {
-        //         res
-        //             .json({
-        //                 type: 'Treatment created',
-        //                 data: data
-        //             })
-        //             .status(200)
-        //     })
-        //     .catch(err => {
-        //         console.log(`caugth error: ${err}`);
-        //         return res.status(500).json(err);
-        //     })
     }
+
+        // Guardar
+    
+        
 }
 
 
