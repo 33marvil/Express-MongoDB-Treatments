6c6da6f4fa8fadea3e0fbd7a76b69a0ed12765c1
33marvil <martinramirezcruz71@gmail.com>
2019-08-13 20:06:06 -0500

    Endpoint-Treatment-Create-Save-Data-v1

--- src/controllers/Treatments/treatments.js
+++ src/controllers/Treatments/treatments.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Treatment = require('../../models/Treatment');
-
+// const Appointment = require('../../models/Appointment');
 const User = require('../../models/User');
 console.log(User)
 
@@ -15,22 +15,45 @@ const controller = {
             // console.log(user);
         // existe el usuario que recivo del body?
         // si existe el usuario
+        // crear  treatment y lo grabo con los datos que tengo
         User
             .findById(userId)
             .exec()
-            .then(user => {
-                console.log(user)
+            .then(User => {
+                // console.log(user)     
+                // Create Object Treatment           
+                  const newTreatment = new Treatment ({
+                      _id: mongoose.Types.ObjectId(),
+                      description: description,
+                      listOfTreatments: listOfTreatments,                     
+                      user: userId
+                  });
+                  console.log(newTreatment);                                      
+                  newTreatment
+                  .save()
+                  .then(treatment => { // promesa de .save()
+                    res
+                      .json({
+                        type: 'Treatment created',
+                        data: treatment
+                      })
+                      .status(200)
+                      })
+                  .catch(err => {
+                    console.log(`caugth error: ${err}`);
+                    return res.status(500).json(err);
+                  })                  
             })
             .catch(err => {
                 console.log(`caugth error: ${err}`);
                 return res.status(500).json(err);
             })
             
-        // crear  treatment y lo grabo con los datos que tengo
+            
         // utilizo propiedad de Treatment -- appointments:  push[id appointment]
 
 
-        // Formar el Objeto Treatment
+        
         
     }
 
